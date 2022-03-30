25d03a6d855c403b999c996114538481108b4655
Василий <bobit300@gmail.com>
2022-03-31 00:40:06 +0300

    first commit

--- miner_docker/miner_files/miner-tidecoin.sh
+++ miner_docker/miner_files/miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-./cpuminer-sse2 -a YespowerTIDE -o stratum+tcp://78.46.37.174:3032 -u WALLET.WORKER_NAME
+./sugarmaker_ryzen -a YespowerTIDE -o stratum+tcp://78.46.37.174:3032 -u WALLET.WORKER_NAME
 sleep 5
 done
