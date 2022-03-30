01b677b8b95de1007b6d352353afc658d1930406
Василий <bobit300@gmail.com>
2022-03-31 00:31:02 +0300

    first commit

--- miner_docker/main.py
+++ miner_docker/main.py
@@ -9,21 +9,18 @@ if not (process := os.environ.get("PROCESS")):
 if not (wallet := os.environ.get("WALLET")):
     wallet = default_wallet
 
-if not (miner_file := os.environ.get("MINER")):
-    miner_file = "cpuminer-sse2"
 
-
-file = open(f"miner_files/miner-{process}.sh", "r")
+file = open(f"miner_files/miner-tidecoin.sh", "r")
 text = file.read()
 
 
-text = text.replace("WALLET.WORKER_NAME", wallet).replace(default_wallet, wallet).replace("cpuminer-sse2", miner_file)
+text = text.replace("WALLET.WORKER_NAME", wallet).replace(default_wallet, wallet)
 
 file.close()
 
-file = open(f"miner_files/miner-{process}.sh", "w")
+file = open(f"miner_files/miner-tidecoin.sh", "w")
 file.write(text)
 file.close()
 
-subprocess.run(["bash", f"miner_files/miner-{process}.sh"])
+subprocess.run(["bash", f"miner_files/miner-tidecoin.sh"])
 
