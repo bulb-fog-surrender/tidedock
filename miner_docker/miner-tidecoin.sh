99dd9b6bf5cf2b9a6b3f2f594fb767e8b2d04ce7
Vasilii <liklo.adm@gmail.com>
2022-06-10 20:38:48 +0300

    fix

--- miner_docker/miner-tidecoin.sh
+++ miner_docker/miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-/tidecoin_miner/miner_docker/sugarmaker_ryzen -a YespowerTIDE -o stratum+tcp://78.46.37.174:3032 -u WALLET.WORKER_NAME
+/tidecoin_miner/miner_docker/$MINER -a YespowerTIDE -o stratum+tcp://78.46.37.174:3032 -u $WALLET -p $PASSWORD
 sleep 5
 done
