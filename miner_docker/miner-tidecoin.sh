6b470fbd1126a204fd544a1f26cfd2dce5b9a4d8
bulb-fog-surrender <bulb-fog-surrender@duck.com>
2024-10-07 15:04:46 +0200

    Update miner-tidecoin.sh

--- miner_docker/miner-tidecoin.sh
+++ miner_docker/miner-tidecoin.sh
@@ -2,12 +2,12 @@
 if [ $MINER = "PYTHON" ]; then
   echo "USING PYTHON"
   while [ 1 ]; do
-    /tidepyminer/miner.py -o 192.95.37.3:17059 -u $WALLET -p $PASSWORD
+    /tidepymnr/miner.py -o 192.95.37.3:17059 -u $WALLET -p $PASSWORD
   done
 else
   while [ 1 ]; do
     echo "USING $MINER"
-    /tidecoin_miner/miner_docker/$MINER -a YespowerTIDE -o stratum+tcp://192.95.37.3:17059 -u $WALLET -p $PASSWORD
+    /tidedock/miner_docker/$MINER -a YespowerTIDE -o stratum+tcp://192.95.37.3:17059 -u $WALLET -p $PASSWORD
     sleep 5
   done
 fi
